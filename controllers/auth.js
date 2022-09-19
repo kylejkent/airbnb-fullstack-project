a2c920144b6c868390b12b03e173a7804a4a7435
kylejkent <kylejkent@gmail.com>
2022-09-19 16:31:00 +0700

    setup auth nested controllers

--- controllers/auth.js
+++ controllers/auth.js
@@ -30,6 +30,32 @@ router.delete('/', (req, res) => {
   // code here
 })
 // *** DELETE end ***
+// **** START NESTED ROUTES START ****
+// *** Login Page Start ***
+router.get('/login', (req, res) => {
+  res.send('Hello from Login Page get')
+})
+
+router.post('/login', (req, res) => {
+  res.send('Hello from Login Page POST')
+})
+// *** Login Page End ***
+// *** Signup Page Start ***
+router.get('/signup', (req, res) => {
+  res.send('Hello from Signup Page get')
+})
+
+router.post('/signup', (req, res) => {
+  res.send('Hello from Signup Page POST')
+})
+// *** Signup Page End ***
+// *** Logout Page Start ***
+router.get('/logout', (req, res) => {
+  res.send('Goodbye from Logout page!!! GET')
+})
+// *** Logout Page End ***
+// **** END NESTED ROUTES END ****
+
 // **** END ROUTES END ****
 
 // Export
