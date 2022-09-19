4b535d0df85e5105b07e5e1102b90c829aed35a8
kylejkent <kylejkent@gmail.com>
2022-09-19 16:40:01 +0700

    add nested controllers to houses

--- controllers/houses.js
+++ controllers/houses.js
@@ -9,7 +9,7 @@ const router = express.Router()
 // **** START ROUTES START ****
 // *** GET start ***
 router.get('/', (req, res) => {
-  res.send('Hello from multiple Houses Page')
+  res.send('Hello from multiple Houses Page GET')
 })
 // *** GET end ***
 
@@ -30,6 +30,31 @@ router.delete('/', (req, res) => {
   // code here
 })
 // *** DELETE end ***
+// **** START NESTED ROUTES START ****
+// *** Create Page Start ***
+router.get('/create', (req, res) => {
+  res.send('Hello from Create Page GET')
+})
+// *** Create Page End ***
+// *** ID Page Start ***
+router.get('/:id', (req, res) => {
+  res.send('Hello from ID Page GET')
+})
+
+router.patch('/:id', (req, res) => {
+  res.send('Hello from ID Page PATCH')
+})
+
+router.delete('/:id', (req, res) => {
+  res.send('Hello from ID Page DELETE')
+})
+// ** Edit ID Start**
+router.get('/:id/edit', (req, res) => {
+  res.send('Hello from ID EDIT Page GET')
+})
+// ** Edit ID End **
+// *** ID Page End ***
+// **** END NESTED ROUTES END ****
 // **** END ROUTES END ****
 
 // Export
