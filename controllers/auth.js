7557ff689e706675a5a5e895f57748b77f8a7b10
kylejkent <kylejkent@gmail.com>
2022-09-20 18:56:31 +0700

    fix errors and console logs

--- controllers/auth.js
+++ controllers/auth.js
@@ -44,8 +44,8 @@ router.post('/login', (req, res) => {
 router.get('/signup', (req, res) => {
   res.render('signup')
 })
-
-router.post('/signup', async (req, res) => {
+// *** signup page end ***
+router.post('/signup', async (req, res, next) => {
   try {
     // *** start define user ***
     let user = {
@@ -55,19 +55,26 @@ router.post('/signup', async (req, res) => {
       password: req.body.profilePassword
     }
     // *** end define user ***
+    // *** start USER EXISTS ***
     // *** Start check if user already ***
-    console.log('USERS MATCHED BELOW')
-    console.log(await Users.countDocuments({ email: user.email }))
-
     if ((await Users.countDocuments({ email: user.email })) > 0) {
-      // *** start USER EXISTS ***
-      res.send(
-        'BAD!!! MATCHES FOUND' +
+      console.log(
+        'BAD!!! MATCHING USER FOUND' +
           ' Num Found: ' +
           ((await Users.countDocuments({ email: user.email })) +
             ' User Email: ' +
             user.email)
       )
+      {
+        throw new Error(
+          'User with this email already exists' +
+            'BAD!!! MATCHES FOUND' +
+            ' - Num Found: ' +
+            ((await Users.countDocuments({ email: user.email })) +
+              ' User Email: ' +
+              user.email)
+        )
+      }
       // *** end USER EXISTS ***
     } else {
       // *** start NEW USER ***
@@ -80,8 +87,7 @@ router.post('/signup', async (req, res) => {
           throw err
         }
       })
-      // console.log(req.user)
-      console.log('LOGGED IN')
+      console.log('LOGGED IN USER:  ' + req.user)
       res.redirect('/houses')
       // *** End handle Signup
       // *** end NEW USER ***
