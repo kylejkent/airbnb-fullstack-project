4cc32ec52c87be52dcfc758de4c9514fbd970055
kylejkent <kylejkent@gmail.com>
2022-09-20 14:32:54 +0700

    Create houses model

---
+++ models/houses.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose')
+const ObjectId = mongoose.Schema.Types.ObjectId
+
+let housesModel = mongoose.model('users', {
+  description: {
+    type: String,
+    required: true
+  },
+  host: {
+    type: ObjectId,
+    ref: 'users',
+    required: true
+  },
+  location: {
+    type: String,
+    required: true
+  },
+  photos: [
+    {
+      type: String
+    }
+  ],
+  price: {
+    type: Number,
+    required: true
+  },
+  room: {
+    type: Number,
+    required: true
+  },
+  title: {
+    type: String,
+    required: true
+  }
+})
+module.exports = housesModel
