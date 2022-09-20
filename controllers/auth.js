4f4ca3257358a790299b87d018f24c9ee1c066ab
kylejkent <kylejkent@gmail.com>
2022-09-20 18:17:52 +0700

    setup signup check

--- controllers/auth.js
+++ controllers/auth.js
@@ -46,7 +46,49 @@ router.get('/signup', (req, res) => {
 })
 
 router.post('/signup', async (req, res) => {
+  try {
+    // *** start define user ***
+    let user = {
+      avatar: req.body.profilePicture,
+      email: req.body.profileEmail,
+      name: req.body.profileFullname,
+      password: req.body.profilePassword
     }
+    // *** end define user ***
+    // *** Start check if user already ***
+    console.log('USERS MATCHED BELOW')
+    console.log(await Users.countDocuments({ email: user.email }))
+
+    if ((await Users.countDocuments({ email: user.email })) > 0) {
+      // *** start USER EXISTS ***
+      res.send(
+        'BAD!!! MATCHES FOUND' +
+          ' Num Found: ' +
+          ((await Users.countDocuments({ email: user.email })) +
+            ' User Email: ' +
+            user.email)
+      )
+      // *** end USER EXISTS ***
+    } else {
+      // *** start NEW USER ***
+      console.log('USER TO CREATE')
+      // *** Start handle signup ***
+      let userCreate = await Users.create(user)
+      let loggedUser = await Users.findOne({ email: user.email })
+      req.login(loggedUser, err => {
+        if (err) {
+          throw err
+        }
+      })
+      // console.log(req.user)
+      console.log('LOGGED IN')
+      res.redirect('/houses')
+      // *** End handle Signup
+      // *** end NEW USER ***
+    }
+  } catch (err) {
+    next(err)
+  }
 })
 // *** Signup Page End ***
 // *** Logout Page Start ***
