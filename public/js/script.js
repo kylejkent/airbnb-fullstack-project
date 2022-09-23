39931b67d7cfdbc7de21d2c29d05d68124dcefde
kylejkent <kylejkent@gmail.com>
2022-09-23 13:54:58 +0700

    setup photo selection

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+let choosePhoto = e => {
+  document.querySelector('e.src')
+  console.log(e.src)
+  let img = document.querySelector('#MainPhoto')
+  img.src = e.src
+}
+// let a = document.querySelector('#photoList').innerHTML
+console.log(a)
